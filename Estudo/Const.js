91132c2d6d07a1b55caec32878c513896cd58288
Shystra <jlsp.2019@hotmail.com>
2023-03-09 19:45:46 -0300

    Test Const

---
+++ Estudo/Const.js
@@ -0,0 +1,12 @@
+const primeiroNumero = 5;
+const segundoNumero = 11;
+
+console.log();
+
+const resultado = primeiroNumero * segundoNumero;
+console.log (resultado); 
+
+const resultado2 = resultado - 50
+console.log (resultado2)
+
+
---
+++ Estudo/Variaveis.js
@@ -0,0 +1,12 @@
+//let nome = 'joao';
+//console.log (nome, 'Nasceu em 1984');
+
+//Podemos fazer da seguinte forma tbm//
+
+let nome; // Declarou a Variavel
+nome = 'Lucas';
+console.log (nome);
+
+nome = 'Souza';
+console.log (nome);
+
