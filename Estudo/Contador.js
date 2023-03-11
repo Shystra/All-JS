44e7c030e3c1aaf828bdf8a0fd72c9ad90b3cf18
Shystra <jlsp.2019@hotmail.com>
2023-03-10 22:08:47 -0300

    Test

---
+++ Estudo/Contador.js
@@ -0,0 +1,21 @@
+let contador = 1;
+
+++ contador; // 2
+++ contador; // 3
+
+console.log (contador);
+
+console.log (-- contador);
+
+console.log (++ contador);
+
+console.log();
+// or we can //
+
+let contador_Exemplo = 8;
+contador_Exemplo *= 2;
+console.log(contador_Exemplo);
+contador_Exemplo *= 2;
+console.log (contador_Exemplo);
+contador_Exemplo *= 2;
+console.log (contador_Exemplo);
---
+++ Estudo/Script.js
@@ -0,0 +1,7 @@
+alert ('Atenção!!! Isso é um teste');
+
+window.prompt ('Digite o seu nome:');
+
+confirma ('Confirmar algo');
+
+const confirma = confirm ('Realmente deseja apagar?');
