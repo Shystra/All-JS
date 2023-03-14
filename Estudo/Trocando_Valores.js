8451932bb5e4401cbf31b6dcbd88a0b747193b01
Shystra <jlsp.2019@hotmail.com>
2023-03-13 21:02:33 -0300

    test

---
+++ Estudo/Trocando_Valores.js
@@ -0,0 +1,15 @@
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+
+const variaveis = [varA, varB, varC]
+
+
+// Trocar os valores, trazendo A para o C e vice versa
+
+const armazena = [varA, varB, varC] = [varB, varC, varA]
+
+console.log(variaveis)
+console.log()
+console.log(armazena);
+
