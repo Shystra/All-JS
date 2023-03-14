4631a1ca5a6d717beaec884678e77853e1ce43b7
Shystra <jlsp.2019@hotmail.com>
2023-03-14 20:07:58 -0300

    test

---
+++ Estudo/Tratando_Numeros.js
@@ -0,0 +1,6 @@
+let num1 = 10.25; // Number
+let num2 = 2.5; // Number
+
+console.log (num1.toString() + num2);  // toString transforma um numero em String;
+
+console.log (num.toFixed(2)); // Mostra ao usuario o resultado (O 2 significa as casas decimais)
