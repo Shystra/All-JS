319ff288a6df0f2e12b312a5eb2c0f2db5961da6
Shystra <jlsp.2019@hotmail.com>
2023-03-16 20:03:20 -0300

    if_else

--- Estudo/True_False.js
+++ Estudo/True_False.js
@@ -1,3 +1,12 @@
+//Operadores Logicos
+
+// && -> and 
+// || -> or
+// ! -> not
+
+console.log('Test' && 'Maria'); // Ele checa e retorna o ultimo valor verdadeiro
+// Caso ele encontre 1 falso ele retorna esse falso
+
 function teste (){
     return 'deu certo';
 }
@@ -6,4 +15,13 @@ const vaiExecutar = 'Teste';
 console.log(vaiExecutar && teste());
 
 
-console.log (0 || false || null || 'test' || true);
+console.log (0 || false || null || 'test' || true); // Retorna apenas o primeiro valor verdadeiro
+
+
+
+
+
+
+
+
+
