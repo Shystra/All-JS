b3fa89879b1a1fb013d1f1ab7b63acfea30c1ddd
Shystra <jlsp.2019@hotmail.com>
2023-03-31 11:19:15 -0300

    Executando condições para validar os valores no formalario

--- formulario/assets/js/main.js
+++ formulario/assets/js/main.js
@@ -4,5 +4,34 @@ const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log('Test Ok');
+    const inputPeso = event.target.querySelector('#peso');
+    const inputAltura = event.target.querySelector('#altura');
+    
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    // Se o peso for diferente
+    if (!peso) {
+        setResultado('Peso Inválido');
+        return
+    }
+
+    console.log(peso, altura);
 });
+
+
+
+    //TODO se o peso invalido ele vai parar aqui
+
+
+function setResultado(msg, isValid) {
+    //TODO O que mandar no setResultado ele vai colocar no html div RESULTADO
+    const resultado = document.querySelector ('#resultado');
+    //TODO Toda vez que chamar o resultado ele vai zerar
+    resultado.innerHTML = '';
+
+    const p = criaP();
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+   
+}
