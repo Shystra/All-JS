48ec07091925a5c32f9903f38ea5b071163a7f9f
Shystra <jlsp.2019@hotmail.com>
2023-04-01 16:06:11 -0300

    Criando functions com data

---
+++ Exercicio_Data/assets/js/main.js
@@ -0,0 +1,41 @@
+const h1 = document.querySelector('.container h1');
+const data = new Date();
+//h1.innerHTML = data.toLocaleDateString('pt-BR', { dateSyle: 'full', timeStyle:'short' });
+
+
+function getDiaSemanaTexto(diaSemana) {
+    const diasSemana = [
+        'domingo', 'segunda', 'terça', 'quarta',
+        'quinta', 'sexta', 'sábado'
+    ];
+    return diasSemana [diaSemana]
+}
+
+function getNomesMes(numeroMes) {
+    const meses = [
+        'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
+        'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
+    ];
+    return meses [numeroMes];
+}
+
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+
+function criaData(data) {
+    const diaSemana = data.getDay();
+    const numeroMes = data.getMonth();
+
+    const nomeDia = getDiaSemanaTexto(diaSemana);
+    const nomeMes = getNomesMes(numeroMes);
+
+    return (
+        `${nomeDia}, ${data.getDate()} de ${nomeMes}` +
+        ` de ${data.getFullYear()} `+
+        `${zeroAEsquerda (data.getHours())}:${zeroAEsquerda(data.getMinutes())}`
+    );
+}
+
+h1.innerHTML = criaData(data)
