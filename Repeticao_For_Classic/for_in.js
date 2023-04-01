0433b45a251d95df46eaa1c60c2f9400851618aa
Shystra <jlsp.2019@hotmail.com>
2023-04-01 18:16:33 -0300

    For in buscando em chaves.

--- Repeticao_For_Classic/for_in.js
+++ Repeticao_For_Classic/for_in.js
@@ -11,4 +11,16 @@ for (let i = 0; i < frutas.length; i++){
 // For com in lê os indices ou chaves dos objetos
 for (let indice in frutas) {
     console.log (frutas[indice]);
-}
+};
+
+
+const pessoa = {
+    nome: 'Lucas',
+    sobrenome: 'Souza',
+    idade: 22
+};
+
+// For in buscando em chaves;
+for (let chaves in pessoa) {
+    console.log (chaves, pessoa[chaves])
+};
