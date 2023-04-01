989ecd5bb61865a19291cb0cf5d0483abd4d3c7e
Shystra <jlsp.2019@hotmail.com>
2023-04-01 17:14:59 -0300

    Percorrendo arrays com for

--- Repeticao_For_Classic/for.js
+++ Repeticao_For_Classic/for.js
@@ -20,3 +20,12 @@ for (let i = 0; i <= 10; i++) {
     const par = i % 2 === 0 ? 'par' : 'impar';
     console.log(i, par)
 }
+
+// Exemplo = Percorrendo Arrays
+
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+console.log (frutas.length);  // Faka o numero de arrays
+
+for (let i = 0; i < frutas.length; i++){
+    console.log(`índice ${i}` ,frutas[i]);
+}
