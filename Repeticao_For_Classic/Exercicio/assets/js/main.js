d9fbb5e4878d326e4e8476917dfc47825a3ddf8b
Shystra <jlsp.2019@hotmail.com>
2023-04-01 17:49:41 -0300

    - const container seleciona o h1 no HTML - const div cria o elemento div - o for foi criado com base nos elementos - laço incluido e injetado na div

---
+++ Repeticao_For_Classic/Exercicio/assets/js/main.js
@@ -0,0 +1,22 @@
+const elementos = [
+    {tag: 'p', texto: 'Qualquer texto'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'section', texto: 'Frase 3'},
+    {tag: 'footer', texto: 'Frase 4'},
+];
+
+// Pegando o elemento .container da HTML
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+//
+
+
+for (let i = 0; i < elementos.length; i++) {
+    const { tag, texto} = elementos[i];
+    const tagCriada = document.createElement(tag);
+    tagCriada.innerHTML = texto;  // Dessa forma ele vai criar um texto no elemento
+    div.appendChild(tagCriada); //Na div ele vai adicionar a tag criada
+    console.log(tag)
+}
+
+container.appendChild (div);
