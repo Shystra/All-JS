373c92fa35e738a765ff80040a339f091c1dfcc7
Shystra <jlsp.2019@hotmail.com>
2023-04-01 17:08:29 -0300

    Criando for

---
+++ Repeticao_For_Classic/for.js
@@ -0,0 +1,14 @@
+// bloco do for nao tem ; no final
+
+// Dentro do parentes sera necessario
+// 1° Criar uma variavel
+// 2° Criar uma condição que vai parar esse laço
+// 3° Inclementar ou decrementar uma variavel de controle
+
+
+//    variavel/condicao/a cada volta no laço esse i vai ser iclementado de 1
+
+
+for (let i = 400; i <= 500; i++) {
+    console.log(`Linha ${i}`);
+}
