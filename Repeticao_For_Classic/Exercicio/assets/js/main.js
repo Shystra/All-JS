302835cd06a275c79ec7392e19d4074ee740cbec
Shystra <jlsp.2019@hotmail.com>
2023-04-01 17:50:57 -0300

    Alterando no innerText (Mais seguro)

--- Repeticao_For_Classic/Exercicio/assets/js/main.js
+++ Repeticao_For_Classic/Exercicio/assets/js/main.js
@@ -14,7 +14,7 @@ const div = document.createElement('div');
 for (let i = 0; i < elementos.length; i++) {
     const { tag, texto} = elementos[i];
     const tagCriada = document.createElement(tag);
-    tagCriada.innerHTML = texto;  // Dessa forma ele vai criar um texto no elemento
+    tagCriada.innerText = texto;  // Dessa forma ele vai criar um texto no elemento
     div.appendChild(tagCriada); //Na div ele vai adicionar a tag criada
     console.log(tag)
 }
