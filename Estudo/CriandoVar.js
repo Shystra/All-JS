70f2851b333e618b3f252199eabf513bb712ea2d
Shystra <jlsp.2019@hotmail.com>
2023-04-01 16:47:53 -0300

    Pegando numeros em chaves

--- Estudo/CriandoVar.js
+++ Estudo/CriandoVar.js
@@ -14,3 +14,12 @@ const numeros = [1000,2000,3,4,5,6,7,8,9];
 const [um, , tres , , cinco] = numeros;
 console.log(um, tres, cinco);
 //console.log(resto)
+
+
+// Formas de Acessar com chaves fechadas
+
+//                          0          1          2                            
+//                       0  1  2    0  1  2    0  1  2                  
+const numerosTeste =  [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+console.log(numerosTeste[2][1]);
+
