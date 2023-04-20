7a1ca0dd248bbe6a2726d02dcbe35be8fa56154e
Shystra <jlsp.2019@hotmail.com>
2023-04-20 17:54:26 -0300

    random numbers

--- Repeticao_While/index.js
+++ Repeticao_While/index.js
@@ -1,6 +1,16 @@
 let i = 0
 
-while (i <= 10) {
+while (i <= 3) {
     console.log(i);
     i++;
 }
+
+// Criando numeros aleatorios
+
+function random(min, max) {
+    const numero = Math.random() * (max - min) + min;
+    return numero;
+}
+
+let rand = random(1, 50);
+console.log(rand);
