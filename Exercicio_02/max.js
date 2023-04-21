a264367800ac2ad897faa53a1922523cae1a705c
Shystra <jlsp.2019@hotmail.com>
2023-04-21 14:38:16 -0300

    make function

--- Exercicio_02/max.js
+++ Exercicio_02/max.js
@@ -1,4 +1,9 @@
-
 function max(x, y) {
     return x > y ? x : y;  //Se x for maior que Y return x se nao ":" return y
 }
+
+const maxExemplo2 = (x, y) => {
+    return x > y ? x : y; //Se x for maior que Y return x se nao ":" return y
+}
+
+console.log(maxExemplo2(100, 200))
