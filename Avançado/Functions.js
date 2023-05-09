d44676cf74af4a0c326a99cf901805860c89ddc5
Shystra <jlsp.2019@hotmail.com>
2023-05-08 21:27:41 -0300

    meaking a function with destroction

--- Avançado/Functions.js
+++ Avançado/Functions.js
@@ -9,3 +9,26 @@ function soma() {
 
 };
 soma(1,2,3,4,5)
+
+
+console.log()
+//Destruction
+
+function conta(operador, acumulador, ...numeros){
+    console.log(operador, acumulador, numeros);
+}
+
+conta('+', 0, 20, 30, 40)
+console.log()
+
+function contaExemploTwo(operador, acumulador, ...numeros) {
+    for(let numero of numeros) {
+        if (operador === '+') acumulador += numero;
+        if (operador === '-') acumulador -+ numero;
+        if (operador === '/') acumulador /= numero;
+        if (operador === '*') acumulador *= numero;
+    }
+    console.log(acumulador)
+}
+
+contaExemploTwo('+', 10, 20)
