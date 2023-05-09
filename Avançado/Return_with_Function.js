48d72601d2ee0f96ac1abd492d5e01bc4efb6ffc
Shystra <jlsp.2019@hotmail.com>
2023-05-08 22:03:34 -0300

    function with return

--- Avançado/Return_with_Function.js
+++ Avançado/Return_with_Function.js
@@ -6,3 +6,26 @@ function soma (a, b){
     return a + b;
 }
 console.log(soma(2, 5))
+
+//_________________________________________//
+
+
+function criaPessoa(nome, sobrenome) {
+    return { nome, sobrenome};
+}
+
+const p1 = criaPessoa('Lucas', 'Souza');
+console.log(p1)
+//_________________________________________//
+
+
+
+function criaMultiplicador (multiplicador) {
+    return function(n) {
+        return n * multiplicador;
+    };
+}
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+console.log(duplica(2))
+console.log(triplica(3))
