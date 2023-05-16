64cd1b6b82f13483c41c03d467665378b654dc44
Shystra <jlsp.2019@hotmail.com>
2023-05-15 21:29:56 -0300

    creating calculation for BMI(IMC)

---
+++ Avançado/factoryFanctions.js
@@ -0,0 +1,21 @@
+function peopleMake (name, lastName, w, k) {
+    return {
+        name, 
+        lastName, 
+
+        talk(coment) {
+            return `${this.name} is ${coment}.`;
+        },
+
+        width: w,
+        kg: k,
+        imc(){
+            const indice = this.width / (this.width ** 2);
+            return indice.toFixed(2);
+        }
+        
+    };
+}
+
+const exemple = peopleMake( 'Lucas', 'Souza', 1.74, 65 );
+console.log(exemple)
