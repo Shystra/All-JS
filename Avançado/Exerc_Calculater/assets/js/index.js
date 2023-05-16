913f90561811749af90681dfbde81f2b2305483b
Shystra <jlsp.2019@hotmail.com>
2023-05-16 20:40:20 -0300

    creating script to calculater

--- Avançado/Exerc_Calculater/assets/js/index.js
+++ Avançado/Exerc_Calculater/assets/js/index.js
@@ -1,8 +1,90 @@
 function criaCalculadora() {
     return {
-        inicia: function() {
-            alert()
+
+        display: document.querySelector('.display'),
+        btnClear: document.querySelector('.btn-clear'),
+
+        inicia() {
+            this.cliqueBotoes();
+            this.pressionaEnter();
         },
+
+
+
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+               if (e.keyCode === 13){
+                this.realizaConta();
+               } 
+            });
+        },
+
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval();
+
+                if(!conta) {
+                    alert('Conta Inválida');
+                    return;
+                }
+
+                this.display.value = String(conta);
+            } catch(e){
+                alert('Conta Inválida');
+                return
+            }
+        },
+
+
+
+
+
+        clearDisplay() {
+            this.display.value = ''
+        },
+
+        apagaUm() {
+            this.display.valu = this.display.value.slice(0, -1);
+        },
+
+
+    
+
+        
+        cliqueBotoes() {
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if(el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                }
+
+                if(el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
+ 
+
+
+
+            });
+        },
+
+        btnParaDisplay(valor){
+            this.display.value += valor;
+        }
+
     };
 }
 
