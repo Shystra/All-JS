7e92c31fc27e68ff0648f420c3cd061a97cb3485
Shystra <jlsp.2019@hotmail.com>
2023-05-16 19:29:26 -0300

    just get and set

--- Avançado/factoryFanctions.js
+++ Avançado/factoryFanctions.js
@@ -1,21 +1,37 @@
-function peopleMake (name, lastName, w, k) {
+function criaPessoa( nome, sobrenome, a, p) {
     return {
-        name, 
-        lastName, 
+        nome, sobrenome,
 
-        talk(coment) {
-            return `${this.name} is ${coment}.`;
+        altura: a,
+        peso: p, 
+
+
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
         },
 
-        width: w,
-        kg: k,
-        BMI () {
-            const indice = this.width / (this.width ** 2);
-            return indice.toFixed(2);
-        }
+        set nomeCompleto (valor) {
+            //tira o valor
+            valor = valor.split(' ');
+            //acrescenta o valor
+            this.nome = valor.shift();
+            this.sobrenome = valor.join(' ');
+
+            console.log(valor);
+        },
+
+        get imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);            
+        },
         
     };
 }
 
-const exemple = peopleMake( 'Lucas', 'Souza', 1.74, 65 );
-console.log(exemple)
+const p1 = criaPessoa ('Lucas', 'Souza', 1.74, 65);
+    const p2 = criaPessoa ('Souza', 'Pereira', 1.74, 65);
+        const p3 = criaPessoa ('Veronica', 'Rueli', 1.66, 65);
+
+console.log(p1.imc);
+    console.log(p1.imc);
+        console.log(p1.imc);
