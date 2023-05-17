13bd72d1948ac54ef59fd3b05eb10ac66201f7b0
Shystra <jlsp.2019@hotmail.com>
2023-05-17 20:09:51 -0300

    just concat -

--- Avançado/concatenar.js
+++ Avançado/concatenar.js
@@ -4,5 +4,10 @@ const num2 = [4, 5, 6];
 const concatenaNumeros = num1.concat(num2);
 console.log( concatenaNumeros );
 
-const concatenaAdicionaValores = concatenaNumeros.concat([7, 8, 9], 'Lucas');
-console.log( concatenaAdicionaValores )
+const concatenaAdicionaValores = concatenaNumeros.concat([ 7, 8, 9 ], 'Lucas');
+console.log( concatenaAdicionaValores );
+
+console.log();
+
+const espalhaNumeros = [...num1, ...num2, ...[ 10, 20, 30 ], 'Souza'];
+console.log( espalhaNumeros );
