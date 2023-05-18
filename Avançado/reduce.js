ecf61575abdb45c99f4d2b54285412362a3b2704
Shystra <jlsp.2019@hotmail.com>
2023-05-18 20:56:40 -0300

    just reduce in table

--- Avançado/reduce.js
+++ Avançado/reduce.js
@@ -1,5 +1,17 @@
 const numeros = [5, 10, 50, 80, 1, 6, 3, 100, 200];
 
+
+
+const pessoas = [
+    { nome: 'Lucas', idade: 23 },
+    { nome: 'Souza', idade: 22 },
+    { nome: 'Veronica', idade: 22 },
+    { nome: 'Rueli', idade: 60 },
+    { nome: 'Maria', idade: 30 },
+];
+idade = 40
+
+
 const total = numeros.reduce(function ( acumulador, valor, indice, array ){
     acumulador += valor;
     return acumulador;
@@ -8,9 +20,21 @@ const total = numeros.reduce(function ( acumulador, valor, indice, array ){
 console.log( total );
 
 const numeroPar = numeros.reduce(function ( acumulador, valor ) {
-    if( valor % 2 == 0 )
+    if ( valor % 2 == 0 )
     { acumulador += valor };
     return acumulador;
 
 }, 0);
 console.log(numeroPar);
+
+
+const pessoaMaisVelha = pessoas.reduce(function ( acumulador, valor ) {
+    if ( acumulador.idade > valor.idade ) return acumulador;
+    return valor;
+});
+console.log(pessoaMaisVelha)
+
+
+
+
+
