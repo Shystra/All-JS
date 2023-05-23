b86f0931f4f2ee361a3128c1702bb3c97f8756b6
Shystra <jlsp.2019@hotmail.com>
2023-05-23 19:27:16 -0300

    manipuling just protoType

---
+++ Avançado/protoType_Manipulando.js
@@ -0,0 +1,30 @@
+function Produto (nome, preco){
+    this.nome = nome;
+    this.preco = preco;
+};
+
+Produto.prototype.desconto = function(percentual){
+    this.preco = this.preco - (this.preco * (percentual / 100));
+};
+
+Produto.prototype.aumento = function(percentual){
+    this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+const p1 = new Produto('Camisa', 50);
+p1.desconto(50);
+console.log(p1);
+
+
+const p2 = {
+    nome: 'Caneca Exemplo', 
+    preco: 15
+};
+Object.setPrototypeOf(p2, Produto.prototype);
+
+p2.aumento(10);
+console.log(p2)
+
+
+
+
