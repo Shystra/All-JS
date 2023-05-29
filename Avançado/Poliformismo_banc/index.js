1f3dd08a491c11beb5d23cdc46b67d4187fb6987
Shystra <jlsp.2019@hotmail.com>
2023-05-29 20:42:12 -0300

    just banc

--- Avançado/Poliformismo_banc/index.js
+++ Avançado/Poliformismo_banc/index.js
@@ -6,9 +6,9 @@ function Conta ( agencia, conta, saldo ){
 };
 
 Conta.prototype.sacar = function( valor ){
-    if( this.saldo < valor ) {
-        this.verSaldo()
-        return
+    if( valor > this.saldo ) {
+        console.log(`Saldo insuficiente: R$${this.saldo},00`)
+        return;
     };
     this.saldo -= valor;
     this.verSaldo()
@@ -24,8 +24,43 @@ Conta.prototype.verSaldo = function(){
     console.log(`Ag/c: ${this.agencia}/${this.conta} Saldo: R$${this.saldo.toFixed(2)}`)
 };
 
-const conta1 = new Conta(11, 22, 10);
-// console.log(conta1)
-conta1.depositar(11);
-conta1.depositar(200);
-conta1.sacar(100)
+
+function ContaCorrente(agencia, conta, saldo, limite){
+    Conta.call(this, agencia, conta, saldo, limite);
+    this.limite = limite;
+
+
+};
+
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function(valor){
+    if (valor > (this.saldo + this.limite)){
+        console.log(`Saldo insuficiente: R$${this.saldo},00!`);
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+};
+
+const testeConta = new ContaCorrente(11, 22, 0, 100);
+testeConta.depositar(10);
+testeConta.sacar(90);
+testeConta.sacar(90);
+
+
+
+
+
+
+
+
+
+// const conta1 = new Conta(11, 22, 10);
+// // console.log(conta1)
+// conta1.depositar(11);
+// conta1.depositar(200);
+// conta1.sacar(100);
+// conta1.sacar(200);
